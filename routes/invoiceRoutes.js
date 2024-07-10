b643572df7bdb41e52b10f3b96264f005bc99fc5
Mahmoud AL Ahmad <131519568+mahmoudAlAhmad3043@users.noreply.github.com>
2024-07-10 23:29:39 +0400

    Update invoiceRoutes.js

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -2,11 +2,11 @@ import InvoiceController from '../controllers/invoiceController_.js';
 import express from 'express';
 const InvoiceRouter = express.Router();
 
-InvoiceRouter.post('/invoices/createInvoice',InvoiceController.createInvoiceCtrl);
-InvoiceRouter.post('/invoices/editInvoice',InvoiceController.editInvoiceCtrl);
-InvoiceRouter.delete('/invoices/deleteInvoice/:invoice_id',InvoiceController.deleteInvoiceCtrl);
-InvoiceRouter.get('/invoices/AllInvoices',InvoiceController.getAllInvoicesCtrl);
-InvoiceRouter.post('/invoices/sellectedInvoice/:searchWord',InvoiceController.getSellectedInvoiceCtrl);
+InvoiceRouter.post('/invoices/create-invoice',InvoiceController.createInvoiceCtrl);
+InvoiceRouter.post('/invoices/edit-invoice',InvoiceController.editInvoiceCtrl);
+InvoiceRouter.delete('/invoices/delete-invoice/:invoice_id',InvoiceController.deleteInvoiceCtrl);
+InvoiceRouter.get('/invoices/all-invoices',InvoiceController.getAllInvoicesCtrl);
+InvoiceRouter.post('/invoices/sellected-invoice/:searchWord',InvoiceController.getSellectedInvoiceCtrl);
 
 
 export default InvoiceRouter;
