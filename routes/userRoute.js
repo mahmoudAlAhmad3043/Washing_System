f5f0472651c77d44dd8283f14448252041a055c6
Mahmoud AL Ahmad <131519568+mahmoudAlAhmad3043@users.noreply.github.com>
2024-07-10 23:33:07 +0400

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,11 +2,11 @@ import UserController from '../controllers/userController_.js'
 import express from 'express';
 const UsersRouter = express.Router();
 
-UsersRouter.post('/users/signUp',UserController.signUpCtrl);
+UsersRouter.post('/users/sign-up',UserController.signUpCtrl);
 
 UsersRouter.post('/users/login',UserController.loginCtrl);
 
-UsersRouter.delete('/users/deleteAccount',UserController.deleteAccountCtrl);
+UsersRouter.delete('/users/delete-account',UserController.deleteAccountCtrl);
 
 
 export default UsersRouter;
