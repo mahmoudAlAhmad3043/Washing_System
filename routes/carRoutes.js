fa953c9074e9cd01d65a8e328659c73097290820
Mahmoud AL Ahmad <131519568+mahmoudAlAhmad3043@users.noreply.github.com>
2024-07-10 23:28:43 +0400

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -2,10 +2,10 @@ import CarController from '../controllers/carController_.js';
 import express from 'express';
 const CarRouter = express.Router();
 
-CarRouter.post('/cars/createCar',CarController.addCarCtrl);
-CarRouter.post('/cars/editCar',CarController.editCarCtrl);
-CarRouter.delete('/cars/deleteCar/:car_id',CarController.deleteCarCtrl);
-CarRouter.get('/cars/getCar/:car_plate/:car_description',CarController.getSellectedCarCtrl);
+CarRouter.post('/cars/create-car',CarController.addCarCtrl);
+CarRouter.post('/cars/edit-car',CarController.editCarCtrl);
+CarRouter.delete('/cars/delete-car/:car_id',CarController.deleteCarCtrl);
+CarRouter.get('/cars/get-car/:car_plate/:car_description',CarController.getSellectedCarCtrl);
 
 
 export default CarRouter;
